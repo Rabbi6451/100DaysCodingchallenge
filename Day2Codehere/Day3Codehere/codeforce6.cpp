58b5379f0b180c0095cb47ce136df7290130a62b
mdrabbi <rabbihmgc2023@gmail.com>
2026-01-23 00:09:28 +0600

    inital change

--- Day2Codehere/Day3Codehere/codeforce6.cpp
+++ Day2Codehere/Day3Codehere/codeforce6.cpp
@@ -1,6 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    char X;
+    cin >> X;
     
+    if (X >= 48 && X <= 57) {          
+        cout << "IS DIGIT" << endl;
+    }
+    else if (X >= 65 && X <= 90) {      
+        cout << "ALPHA" << endl;
+        cout << "IS CAPITAL" << endl;
+    }
+    else if (X >= 97 && X <= 122) {  
+        cout << "ALPHA" << endl;
+        cout << "IS SMALL" << endl;
+    }
+    
+    return 0;
 }
