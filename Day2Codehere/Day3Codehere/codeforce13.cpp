ea0398364d2435d145c3894f0c89f41c17c48dc5
mdrabbi <rabbihmgc2023@gmail.com>
2026-01-24 16:45:37 +0600

    Updated UI

--- Day2Codehere/Day3Codehere/codeforce13.cpp
+++ Day2Codehere/Day3Codehere/codeforce13.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
 using namespace std;
- 
+
 int main(){
-    cout<<"hello world";
-    cout<<"hello world";
+    int a, b;
+    char s;
+    
+    cin >> a >> s >> b;
+    
+    bool is_correct = false;
+    
+    switch(s){
+        case '<':
+            is_correct = (a < b);
+            break;
+        case '>':
+            is_correct = (a > b);
+            break;
+        case '=':
+            is_correct = (a == b);
+            break;
+    }
+    
+    if(is_correct){
+        cout << "Right" << endl;
+    } else {
+        cout << "Wrong" << endl;
+    }
+    
+    return 0;
 }
