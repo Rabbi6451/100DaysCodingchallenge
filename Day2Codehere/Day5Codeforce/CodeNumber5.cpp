3af550de9c21bec06b3b0d3f084ff7e18d4f0852
mdrabbi <rabbihmgc2023@gmail.com>
2026-01-25 23:54:03 +0600

    Updated UI

--- Day2Codehere/Day5Codeforce/CodeNumber5.cpp
+++ Day2Codehere/Day5Codeforce/CodeNumber5.cpp
@@ -1,6 +1,13 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main(){
+int main() {
+    int n, m, a, b;
+    cin >> n >> m >> a >> b;
     
+    
+    cout << min(n * a, min((n + m - 1) / m * b, n / m * b + min((n % m) * a, b)));
+    
+    return 0;
 }
