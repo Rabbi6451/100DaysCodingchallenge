1381e4023b7863cf89fdda76601c07f519d08c8d
mdrabbi <rabbihmgc2023@gmail.com>
2026-01-27 23:45:00 +0600

    Updated UI

--- Day2Codehere/Day6Codehere/CodeNumber4.cpp
+++ Day2Codehere/Day6Codehere/CodeNumber4.cpp
@@ -1,17 +1,23 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    string a;
-    cin>>a;
-
-    reverse(a.begin(),a.end());
-
-    if(a==a){
-        cout<<a<<endl;
-        cout<<"YES";
-    }else{
-        cout<<a<<endl;
-        cout<<"NO";
+int main() {
+    int n;
+    cin >> n;
+    vector<bool> prime(n + 1, true);
+    prime[0] = prime[1] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (prime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                prime[j] = false;
+            }
+        }
     }
-}
+    for (int i = 2; i <= n; i++) {
+        if (prime[i]) {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}   
