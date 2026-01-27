08c33498076efc93856963b5cace219a236626eb
mdrabbi <rabbihmgc2023@gmail.com>
2026-01-27 22:49:32 +0600

    Updated UI

---
+++ Day2Codehere/Day5Codeforce/Codeforce7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    int a;
+
+    vector<int> t(a);
+    for(int i=0;i<a;i++){
+        cin>>t[i];
+
+    }
+}
---
+++ Day2Codehere/Day6Codehere/CodeNumber1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b=12;
+    cin>>a;
+
+    
+        for(int i=1;i<13;i++){
+            cout<<a<<" * "<<i<<" = "<<a*i<<endl;
+        }
+    
+
+
+}
