ca973538d9de711067bd55c37c69213cc2593524
mdrabbi <rabbihmgc2023@gmail.com>
2026-01-27 23:42:43 +0600

    Updated UI

--- Day2Codehere/Day6Codehere/CodeNumberPractice.cpp
+++ Day2Codehere/Day6Codehere/CodeNumberPractice.cpp
@@ -6,23 +6,19 @@ using namespace std;
 int main() {
     string a;
     cin >> a;
-
-    string reversed = a;
-    reverse(reversed.begin(), reversed.end());
-    
-    size_t start = reversed.find_first_not_of('0');
-    if (start == string::npos) { 
-        reversed = "0";
+    string rev = a;
+    reverse(rev.begin(), rev.end());
+    size_t start = rev.find_first_not_of('0');
+    if (start == string::npos) {
+        rev = "0";
     } else {
-        reversed = reversed.substr(start);
+        rev = rev.substr(start);
     }
-
-    cout << reversed << endl;
-    if (a == string(reversed.rbegin(), reversed.rend())) {
+    cout << rev << endl;
+    if (a == rev) {
         cout << "YES";
     } else {
         cout << "NO";
     }
-
     return 0;
 }   
