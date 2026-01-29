03d8036b738db8299308fa2e79a5122df02cc285
mdrabbi <rabbihmgc2023@gmail.com>
2026-01-29 19:22:52 +0600

    fourth

---
+++ Day2Codehere/Day7Codehere/CodeNumber3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    
+    bool found = false;
+    
+    for(int num = A; num <= B; num++) {
+        int temp = num;
+        bool isLucky = true;
+        
+        while(temp > 0) {
+            int digit = temp % 10;
+            if(digit != 4 && digit != 7) {
+                isLucky = false;
+                break;
+            }
+            temp /= 10;
+        }
+        
+        if(isLucky) {
+            cout << num << " ";
+            found = true;
+        }
+    }
+    
+    if(!found) {
+        cout << -1;
+    }
+    
+    return 0;
+}
