8bbadcf1d3daf09d3f0649cce64040c0b36654ce
mdrabbi <rabbihmgc2023@gmail.com>
2026-01-30 22:00:22 +0600

    commit

---
+++ Day2Codehere/Day7Codehere/CodeNumber11.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+
+    vector<long long> t(a);
+
+    for(int i=0;i<a;i++){
+        cin>>t[i];
+    }
+    for(int i=0;i<a;i++){
+        for(int j=0;j<a-1;j++){
+            if(t[j]> t[j+1]){
+                int temp = t[j];
+                t[j] = t[j+1];
+                t[j+1] = temp;
+            }
+        }
+    }
+    for(int i=0;i<a;i++){
+        cout<<t[i]<<" ";
+    }
+}
