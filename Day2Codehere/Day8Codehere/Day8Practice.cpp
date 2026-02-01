141c303debee0a85e75afbd2b99791e161f20908
mdrabbi <rabbihmgc2023@gmail.com>
2026-02-01 23:23:47 +0600

    commit

--- Day2Codehere/Day8Codehere/Day8Practice.cpp
+++ Day2Codehere/Day8Codehere/Day8Practice.cpp
@@ -1,32 +1,48 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-int main(){
-    int k=2;
-    while(--k){
-    int b;
-    cin>>b;
-
-    vector<int> k(b);
-    for(int j=0;j<b;j++){
-        cin>>k[j];
+int main() {
+    int test_cases = 2;
+    
+    while(test_cases--) {
+        int size1;
+        cin >> size1;
+        
+        vector<int> arr1(size1);
+        for(int i = 0; i < size1; i++) {
+            cin >> arr1[i];
+        }
+        
+        int size2;
+        cin >> size2;
+        
+        vector<int> arr2(size2);
+        for(int i = 0; i < size2; i++) {
+            cin >> arr2[i];
+        }
+        
+        if(size1 != size2) {
+            cout << "No" << endl;
+            continue;
+        }
+        
+        // Check if reverse of arr1 equals arr2
+        bool isMatch = true;
+        for(int i = 0; i < size1; i++) {
+            if(arr1[i] != arr2[size1 - 1 - i]) {
+                isMatch = false;
+                break;
+            }
+        }
+        
+        if(isMatch) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
     }
-
-    int a;
-    cin>>a;
-
-    vector<int> t(a);
-
-    for(int i=0;i<a;i++){
-        cin>>t[i];
-    }
-
-    reverse(t.begin(), t.end());
-
-    if(t[i] == t[j]){
-        cout<<"Yes";
-    }else{
-        cout<<"No";
-    }
-}
+    
+    return 0;
 }
