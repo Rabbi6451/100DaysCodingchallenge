84d3b4e01e6f70a8bf63f83cdd9b6ea44abde537
mdrabbi <rabbihmgc2023@gmail.com>
2026-02-01 22:13:14 +0600

    commit

--- Day2Codehere/Day7Codehere/Practice.cpp
+++ Day2Codehere/Day7Codehere/Practice.cpp
@@ -1,1 +1,32 @@
 #include<iostream> 
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+
+    vector<int> t(a);
+    for(int i=0;i<a;i++){
+        cin>>t[i];
+    }
+
+    int minNum = t[0];
+    for(int i=0;i<a;i++){
+        if(t[i] < minNum){
+            minNum = t[i];
+        }
+    }
+
+    int freq = 0;
+    for(int i=0;i<a;i++){
+        t[i] == minNum;
+        freq++;
+    }
+
+    if(freq % 2 == 0){
+        cout<<"Not Lucky";
+    }else if(freq%2 != 0){
+        cout<<"Lucky";
+
+    }
+}
