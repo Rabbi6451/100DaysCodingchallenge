4d19e59f8dc7f5d82f8b71fcdf39d9b1f2e60df7
mdrabbi <rabbihmgc2023@gmail.com>
2026-02-03 23:36:34 +0600

    commit

---
+++ Day2Codehere/Day8Codehere/CodeNumber9.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        int N;
+        cin >> N;
+        vector<long long> A(N);
+        for (int i = 0; i < N; i++) {
+            cin >> A[i];
+        }
+        
+        long long ans = 0;
+        int len = 1;           
+        
+        for (int i = 1; i < N; i++) {
+            if (A[i] >= A[i-1]) {
+                len++;
+            } else {
+                // All subarrays inside previous segment are good
+                ans += (len * 1LL * (len + 1)) / 2;
+                len = 1;
+            }
+        }
+        
+        // Don't forget the last segment
+        ans += (len * 1LL * (len + 1)) / 2;
+        
+        cout << ans << endl;
+    }
+    
+    return 0;
+}
