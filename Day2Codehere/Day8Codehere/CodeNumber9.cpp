f1e78c7abb57043489ce5ac4d447741239d98aa9
mdrabbi <rabbihmgc2023@gmail.com>
2026-02-03 23:40:53 +0600

    commit

--- Day2Codehere/Day8Codehere/CodeNumber9.cpp
+++ Day2Codehere/Day8Codehere/CodeNumber9.cpp
@@ -10,24 +10,23 @@ int main() {
         int N;
         cin >> N;
         
-        vector<long long> A(N);   // using long long to be safe
+        vector<long long> A(N);  
         for (int i = 0; i < N; i++) {
             cin >> A[i];
         }
         
         long long answer = 0;
         
-        // For each possible starting index
         for (int i = 0; i < N; i++) {
-            // At least the single element is always non-decreasing
+           
             answer++;  
             
-            // Try to extend to the right
+            
             for (int j = i + 1; j < N; j++) {
                 if (A[j] >= A[j-1]) {
-                    answer++;        // [i...j] is good
+                    answer++;     
                 } else {
-                    break;           // cannot extend anymore
+                    break;           
                 }
             }
         }
