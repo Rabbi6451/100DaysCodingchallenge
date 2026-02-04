f4abc12c42ed7fb80409163e33702ea34a62c1e7
mdrabbi <rabbihmgc2023@gmail.com>
2026-02-04 23:50:24 +0600

    commit

--- Day2Codehere/Day8Codehere/CodeNumber13.cpp
+++ Day2Codehere/Day8Codehere/CodeNumber13.cpp
@@ -7,7 +7,6 @@ int main() {
     
     int matrix[100][100];
     
-    // Read matrix
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             cin >> matrix[i][j];
@@ -15,21 +14,19 @@ int main() {
     }
     
     int primaryDiagonal = 0;
-    // Primary diagonal: row = column
     for (int i = 0; i < N; i++) {
         primaryDiagonal += matrix[i][i];
     }
     
     int secondaryDiagonal = 0;
-    // Secondary diagonal: row + column = N-1
     for (int i = 0; i < N; i++) {
         secondaryDiagonal += matrix[i][N - 1 - i];
     }
     
-    // Calculate absolute difference
     int difference = primaryDiagonal - secondaryDiagonal;
     if (difference < 0) {
-        difference = -difference;  // Make it positive
+        difference = -difference;  
+        
     }
     
     cout << difference << endl;
