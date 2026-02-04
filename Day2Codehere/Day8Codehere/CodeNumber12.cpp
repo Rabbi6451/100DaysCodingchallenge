1faf6e7681c72d4a404d02702a76f6ed780d99d0
mdrabbi <rabbihmgc2023@gmail.com>
2026-02-04 23:46:16 +0600

    commit

--- Day2Codehere/Day8Codehere/CodeNumber12.cpp
+++ Day2Codehere/Day8Codehere/CodeNumber12.cpp
@@ -7,7 +7,6 @@ int main() {
     
     int arr[100][100];
     
-    // Read the array
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             cin >> arr[i][j];
@@ -17,18 +16,20 @@ int main() {
     int X;
     cin >> X;
     
-    // Search for X
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             if (arr[i][j] == X) {
                 cout << "will not take number" << endl;
-                return 0;  // Exit early if found
+                return 0;  
             }
         }
     }
     
-    // If we reach here, X was not found
     cout << "will take number" << endl;
     
     return 0;
 }
+
+/*
+Input : 
+*/
