bd01a06360655614c8f8deae3e30d81f9a691e29
mdrabbi <rabbihmgc2023@gmail.com>
2026-03-02 18:56:32 +0600

    first

--- Day2Codehere/Practice/Practice1.cpp
+++ Day2Codehere/Practice/Practice1.cpp
@@ -6,7 +6,7 @@ int main() {
     int n, m;
     cin >> n >> m;
     
-    vector<int> a(n), b(m);
+    vector<long long> a(n), b(m);
     for (int i = 0; i < n; i++) cin >> a[i];
     for (int i = 0; i < m; i++) cin >> b[i];
     
