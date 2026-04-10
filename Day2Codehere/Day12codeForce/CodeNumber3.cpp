162b6e78f9f2fbf8091f3b9c247db1ccc347cb60
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-10 15:40:32 +0600

    first

---
+++ Day2Codehere/Day12codeForce/CodeNumber3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int b;
+    double a=3.14;
+    cin>>b;
+    cout<<a*b*b;
+
+    return 0;
+}
---
+++ Day2Codehere/Day12codeForce/CodeNumber4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    a=8;
+    b=4;
+    int c=a+b;
+    int d=a-b;
+    int e=a/b;
+    int f=a*b;
+    cout<< c<<endl<<d<<endl<<e<<endl<<f<<endl;
+    
+}
