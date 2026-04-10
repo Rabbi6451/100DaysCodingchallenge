a78f2e4eb97d4f0d61e31cea46a4b7c56ed25e2d
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-10 23:40:10 +0600

    first

---
+++ Day2Codehere/Day12codeForce/CodeNumber7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    int i;
+
+        for(i=a;i>=1;--i){
+        cout<<i;
+            if(i>1){
+                cout<<" ";
+            }
+        
+    }
+    
+}
