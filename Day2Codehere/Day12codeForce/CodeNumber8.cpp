200e1f20434d6b53ee6388c7bec9e4b918655f0d
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-11 01:57:10 +0600

    first

---
+++ Day2Codehere/Day12codeForce/CodeNumber8.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    while (n--) {
+        int num;
+        cin >> num;
+
+        if (num == 0) {
+            cout << "0";
+        } else {
+            int digits[10];
+            int count = 0;
+            while (num > 0) {
+                digits[count++] = num % 10;
+                num /= 10;
+            }
+            for (int i = count - 1; i >= 0; i--) {
+                cout << digits[i];
+                if (i > 0) cout << " ";
+            }
+        }
+        cout<<endl;
+    }
+}   
