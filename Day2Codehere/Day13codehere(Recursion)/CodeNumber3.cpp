c3d0951868780602a1466627ce4ad38d27f2986f
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-13 00:20:03 +0600

    inital

---
+++ Day2Codehere/Day13codehere(Recursion)/CodeNumber3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int n;
+    cin >> n; 
+    vector<int> nums(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        int x = nums[i];
+        vector<int> binary;
+
+        while (x > 0) {
+            binary.push_back(x % 2);  
+            x /= 2;                  
+        }
+
+        for (int j = binary.size() - 1; j >= 0; j--) {
+            cout << binary[j];
+        }
+        cout << "\n";
+    }
+}
