5ad0707d7a133f18229c2ea4e1af3e4d2119c425
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-13 13:27:56 +0600

    first

--- Day2Codehere/Day13codehere(Recursion)/Day13Practice.cpp
+++ Day2Codehere/Day13codehere(Recursion)/Day13Practice.cpp
@@ -4,21 +4,22 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    vector<int> nums(n);
+    vector<int> t(n);
+    for(int i=0;i<n;i++){
+        cin>>t[i];
+    }
 
     for(int i=0;i<n;i++){
-        cin>>nums[i];
-    }
-    for(int i=0;i<n;i++){
-        int x = nums[i];
+        int x=t[i];
         vector<int> binary;
         while(x>0){
-            binary.push_back(x%2);
-            x/=2;
+        binary.push_back(x%2);
+        x=x/2;
         }
-        for(int j=binary.size()-1;j>=0;j--){
-            cout<<binary[j];
-        }
-        cout<<"\n";
+        for(int j= binary.size()-1;j>=0;j--){
+        cout<<binary[j];
     }
+       
+    cout<<"\n";
+}
 }
